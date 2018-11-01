0d231ea1d7db3debcf3a054bec109c0e810d93c6
Bürgermeister <freak03@gmx.net>
2018-11-01 11:36:31 +0100

    Final Save

--- Assets/Scripts/ScriptableObjects/Statemachine/_Base/Actions/RespawnAction.cs
+++ Assets/Scripts/ScriptableObjects/Statemachine/_Base/Actions/RespawnAction.cs
@@ -18,6 +18,7 @@ public class RespawnAction : Action
     public override void ExitState(StateController controller)
     {
         controller.rigidbody2D.velocity = Vector2.zero;
-        controller.rigidbody2D.MovePosition(lastSavePosition);
+     //   controller.rigidbody2D.MovePosition(lastSavePosition);
+        controller.transform.position = lastSavePosition.Value;
     }
 }
